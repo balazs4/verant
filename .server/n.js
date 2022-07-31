39cf1fcddf9a5af42b165b8a39038a1642a2f39d
balazs4 <balazs4@users.noreply.github.com>
2022-07-31 10:30:26 +0200

    YOLO coMmit :sponge:

--- .server/n.js
+++ .server/n.js
@@ -1,9 +1,20 @@
+import fs from 'fs';
 export default { world };
 
 function world(r) {
+  r.log(r.variables.request_body_file);
+  if (r.method.toLowerCase() === 'post' && r.variables.request_body_file) {
+    fs.promises
+      .readFile(r.variables.request_body_file)
+      // .then((content) => {
+      //   return fs.promises.writeFile('/tmp/random', content);
+      // })
+      .then(() => {
+        return r.return(200, 'ok');
+      });
+    return;
+  }
+
   r.headersOut['content-type'] = 'application/json';
-  r.return(
-    200,
-    JSON.stringify({ timestamp: Date.now(), message: 'Hello World from njs!' })
-  );
+  r.return(200, JSON.stringify({ r }));
 }
